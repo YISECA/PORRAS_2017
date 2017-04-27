9c49e1aea21feace13fcec7971511a60f19bfc52
Daniel Feo <danielandres.feo@gmail.com>
2017-04-27 16:30:30 -0500

    cambio de vista

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -13,7 +13,7 @@
 
     
 
-	<form method="POST" action="insertar" id="form_gen" enctype="multipart/form-data">
+	<form method="POST" action="insertar_persona" id="form_participante" enctype="multipart/form-data">
 
 
 
@@ -24,19 +24,16 @@
     	<div class="panel panel-default">
 
   	<div class="panel-heading">
-
           <h4>Registrar Integrantes al Equipo  o Institución <span style="color:red"> {{$equipo->nombre_institucion}}</span> </h4> 
           <h4>Rango de edad de integrantes en esta categoría: <span style="color:red" id="rango">{{ $equipo->rangoEdad['edad'] }}</span></h4>
           <h4 ># Participantes registrados en este Equipo O Institución : <span style="color:red" id="cuenta"></span></h4>
-   </div>
+    </div>
 
   	<div class="panel-body">
-
-
   	<div class="row">
   	<br>
-  <!-- id del equipo-->	
-<input type="hidden" class="form-control" id="id" name="id" value="{{$equipo->id}}" >
+
+	<input type="hidden" class="form-control" id="id" name="id" value="{{$equipo->id}}" >
 
 
   	<div class="col-md-6"><label class="freebirdFormviewerViewItemsItemItemTitle" for="formGroupExampleInput2">Tipo de Documento <span style="color: red;font-size: 11px;text-transform: capitalize;color:red">(Campo Obligatorio)</span></label></div>
@@ -131,32 +128,26 @@
 
 		</fieldset>
 
-		 <div class="freebirdFormviewerViewFormContent "><div class="freebirdFormviewerViewHeaderHeader"><div class="freebirdFormviewerViewHeaderTitleRow"><div class="freebirdFormviewerViewHeaderTitle" dir="auto" role="heading" aria-level="1"></div></div><div class="freebirdFormviewerViewHeaderRequiredLegend" aria-hidden="true" dir="auto"></div></div><div class="freebirdFormviewerViewItemList" role="list"><div class="freebirdMaterialHeaderbannerLabelContainer freebirdFormviewerViewItemsPagebreakBanner" jsname="bLLMxc" role="heading"><div class="freebirdMaterialHeaderbannerLabelTextContainer freebirdSolidBackground freebirdMaterialHeaderbannerPagebreakBanner"><div class="freebirdMaterialHeaderbannerPagebreakText freebirdFormviewerViewItemsPagebreakBannerText"></div></div></div><div class="freebirdFormviewerViewItemsPagebreakDescriptionText"></div><div role="listitem" class="freebirdFormviewerViewItemsItemItem" jsname="ibnC6b" jscontroller="hIYTQc" jsaction="JIbuQc:qzJD1c;sPvj8e:e4JwSe" data-required="true" data-other-input="qSV85" data-other-hidden="MfYA1e" data-item-id="131124881"><div class="freebirdFormviewerViewItemsItemItemheader"><div class="freebirdFormviewerViewItemsItemItemTitleContainer"><div class="freebirdFormviewerViewItemsItemItemTitle" dir="auto" id="i1" role="heading" aria-level="2" aria-describedby="i.desc.131124881">Términos de inscripción <span class="freebirdFormviewerViewItemsItemRequiredAsterisk" aria-hidden="true">*</span></div><div class="freebirdFormviewerViewItemsItemItemHelpText" id="i.desc.131124881" dir="auto"><br>Usted acepta y cumple con los requisitos exigidos en la presente Resolución</div></div></div><div jsname="JNdkSc" role="group" aria-labelledby="i1" aria-describedby="i.desc.131124881 i.err.131124881 i.req.131124881" class=""><div class="" jsname="MPu53c" jscontroller="GJQA8b" jsaction="JIbuQc:aj0Jcf" data-value="Acepto"><div class="freebirdFormviewerViewItemsCheckboxChoice"><label class="docssharedWizToggleLabeledContainer freebirdFormviewerViewItemsCheckboxContainer"><div class="exportLabelWrapper"><input type="checkbox" required style="float: left;
-
-margin: 0px;" name="acepto" id="acepto"><div class="docssharedWizToggleLabeledContent"><div class="docssharedWizToggleLabeledPrimaryText"><span dir="auto" class="docssharedWizToggleLabeledLabelText freebirdFormviewerViewItemsCheckboxLabel">Acepto</span></div></div></div></label></div><input name="entry.1642827248" jsname="ekGZBc" disabled="" type="hidden"></div></div><div id="i.req.131124881" class="screenreaderOnly"></div><div jsname="XbIQze" class="freebirdFormviewerViewItemsItemErrorMessage" id="i.err.131124881" role="alert"></div></div></div><div class="freebirdFormviewerViewNavigationNavControls" jscontroller="lSvzH" jsaction="rcuQ6b:npT2md;JIbuQc:V3upec(GeGHKb),HiUbje(M2UYVd),NPBnCf(OCpkoe)" data-shuffle-seed="-2327421662174229681"><div class="freebirdFormviewerViewNavigationButtonsAndProgress"><div class="freebirdFormviewerViewNavigationButtons">
-
-
 <div class="form-group">
-  <button type="button" id="enviar"  class="btn btn-info">Registrar participante en el grupo</button>
+<button type="submit" id="inscribir persona"  class="btn btn-info">Registrar participante en el grupo</button>
 </div>
-<input class="enviar" type="submit" value="Enviar">
-
-
-
-</div><div class="freebirdFormviewerViewNavigationProgress"><div class="freebirdFormviewerViewNavigationProgressIndicator" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" aria-labelledby="lpd4pf" role="progressbar"><div class="freebirdFormviewerViewNavigationProgressIndicatorFill done" style="width:100%"></div></div><div id="lpd4pf" class="quantumWizButtonPaperbuttonContent" aria-hidden="true">Página 1 de 1</div></div></div><div class="freebirdFormviewerViewNavigationPasswordWarning">.</div></div>
-
-
-
-	
-
- 
-
-
-
-	</form>
+</form>
 
 	<script type="text/javascript" src="public/Js/form.js" ></script>
-
+	<div class="row" >
+		<div class="col-md-12">
+			<table>
+		 	@foreach($inscritos as $inscrito)
+			 <tr>
+				 <td>
+					 <p>{{$inscrito->cedula}}</p>
+				 </td>
+				 <td><form action="eliminar_participante" method="post" ><input type="hidden" name="cedula" value="{{$inscrito->cedula}}"><input type="hidden" name="equipo" value="{{$equipo->id}}"> <input type="submit" id="eliminar"  value="eliminar"></form></td>
+			 </tr>
+			@endforeach
+			</table>
+		</div>
+	</div>
 @stop
 
 
