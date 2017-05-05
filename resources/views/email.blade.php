fb4089de41a6bbb13d332ef99431b9a588c4be62
Daniel Feo <danielandres.feo@gmail.com>
2017-05-05 08:51:49 -0500

    cambio de texto email

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -2,10 +2,11 @@
 
 <h1>Registro exitoso del equipo </h1>
 
-<h1>a continuación agregue los participantes a través del botón Registrar participantes </h1>
+<h1>A continuación agregue los participantes a través del botón Registrar participantes </h1>
+<p>Tiene 24 horas para agregar minimo 12 participates o su equipo sera eliminado </p>
 
 
-<form method="post" action="http://localhost/PORRAS_2017/insertar_participante">
+<form method="post" action="http://idrd.go.co/PORRAS_2017/insertar_participante">
 <input type="hidden" name="equipo" value="{{$id}}"></input>
 <input type="submit"value="Agregar Participantes"></input>	
 </form>
